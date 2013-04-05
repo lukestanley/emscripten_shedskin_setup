4142a461a0e912cbe45807284792f38317333783
Luke Stanley <luke.stanley@gmail.com>
2013-04-05 08:36:51 -0700

    Compiling Shedskin to JS should now work! Regex and a few thingd need fixing later

--- setup_emscripten_with_shedskin.sh
+++ setup_emscripten_with_shedskin.sh
@@ -8,6 +8,8 @@ export BASEDIRECTORY=$(pwd)
 sudo apt-get -y install g++ cmake build-essential libffi-dev git openjdk-6-jdk nodejs nodejs-legacy libgc-dev libpcre++-dev libpcre3-dev
 
 wget http://llvm.org/releases/3.2/clang+llvm-3.2-x86-linux-ubuntu-12.04.tar.gz
+wget http://dl.dropbox.com/u/33191825/Patches.tar.gz
+tar -xvzf Patches.tar.gz
 git clone git://github.com/kripken/emscripten.git
 git clone git://gitorious.org/shedskin/mainline.git
 mv mainline shedskin_src
@@ -54,21 +56,40 @@ node a.out.js
 
 
 cd $BASEDIRECTORY
+cp /usr/include/gc/gc_cpp.h ./emscripten/system/include/
+cp /usr/include/gc/gc_allocator.h ./emscripten/system/include/
+
+if [ -d "/vagrant" ]; then
+        #cp /vagrant/shedskin_Makefile Makefile
+        patch ./shedskin_src/shedskin/lib/builtin.hpp < /vagrant/builtin.hpp.patch
+fi
+if [ ! -d "/vagrant" ]; then
+    #cp $BASEDIRECTORY/shedskin_Makefile Makefile
+    patch ./shedskin_src/shedskin/lib/builtin.hpp < $BASEDIRECTORY/builtin.hpp.patch
+fi
+
+
+patch ./emscripten/system/include/gc_cpp.h Patches/gc_cpp.h.patch 
+patch ./shedskin_src/shedskin/lib/builtin.cpp Patches/builtin.cpp.patch 
+patch ./shedskin_src/shedskin/lib/builtin.hpp Patches/builtin.hpp.patch 
+patch ./shedskin_src/shedskin/lib/builtin/complex.cpp Patches/complex.cpp.patch 
+patch ./shedskin_src/shedskin/lib/builtin/exception.hpp Patches/exception.hpp.patch 
+cd shedskin_src/
+sudo python setup.py install
+cd ..
 mkdir shedskin_js
 cd shedskin_js
 echo 'print "hello from shedskin in JS"' >> hello.py
+export SHEDSKIN_LIBDIR=/usr/local/lib/python2.7/dist-packages/shedskin/lib
 shedskin hello.py
 rm Makefile
-if [ -d "/vagrant" ]; then
-	cp /vagrant/shedskin_Makefile Makefile
-	patch ../shedskin_src/shedskin/lib/builtin.hpp < /vagrant/builtin.hpp.patch
-fi
-if [ ! -d "/vagrant" ]; then
-    cp $BASEDIRECTORY/shedskin_Makefile Makefile
-    patch ../shedskin_src/shedskin/lib/builtin.hpp < $BASEDIRECTORY/builtin.hpp.patch
-fi
+
+
+
 export PATH=$PATH:$BASEDIRECTORY/emscripten
-make
+emcc hello.cpp $SHEDSKIN_LIBDIR/builtin.cpp -I $SHEDSKIN_LIBDIR
+node a.out.js
+#make
 
 
 
